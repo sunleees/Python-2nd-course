df1cbf3f3625eda81386e499add203d1275f70b2
USER <koyfplaton@gmail.com>
2025-03-14 16:21:06 +0200

    testing ci-cd

--- test_utils.py
+++ test_utils.py
@@ -16,3 +16,14 @@ class TestAddTwoNumbers:
                 number2 = "dfsjdfjsfjk"
                 with pytest.raises(ValueError):
                         add_two_numbers(number1,number2)
+    @pytest.mark.parametrize(
+        'number1, number2, expected_result',
+        [
+            (2, 3, 5),
+            ('23', '23', 46)
+        ]
+
+    )
+    def test_success(self, number1, number2, expected_result):
+        actual_result = add_two_numbers(number1, number2)
+        assert actual_result == expected_result
--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import abc
 import json
 
 
-def add_two_numbers(number1: float, number2: float) -> float:
-    result = number1 + number2
+def add_two_numbers(number1: float | str, number2: float | str) -> float:
+    result = float(number1) + float(number2)
 
     return result
