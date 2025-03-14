7333c708198dfb4f0459d62b90e23cc7ac8054d9
USER <koyfplaton@gmail.com>
2025-03-14 16:38:44 +0200

    testing ci-cd

--- test_utils.py
+++ test_utils.py
@@ -12,17 +12,13 @@ class TestAddTwoNumbers:
         assert actual_result == expected_result, f"fix{add_two_numbers()}"
 
     def test_add_two_numbers_error(self):
-                number1 = "jdfjskdfkjs"
-                number2 = "dfsjdfjsfjk"
-                with pytest.raises(ValueError):
-                        add_two_numbers(number1,number2)
-    @pytest.mark.parametrize(
-        'number1, number2, expected_result',
-        [
-            (2, 3, 5),
-            ('23', '23', 46)
-        ]
+        number1 = "jdfjskdfkjs"
+        number2 = "dfsjdfjsfjk"
+        with pytest.raises(ValueError):
+            add_two_numbers(number1, number2)
 
+    @pytest.mark.parametrize(
+        "number1, number2, expected_result", [(2, 3, 5), ("23", "23", 46)]
     )
     def test_success(self, number1, number2, expected_result):
         actual_result = add_two_numbers(number1, number2)
